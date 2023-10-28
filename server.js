749a01a62f99a8e7fa3db3aedb8768e94468a837
amtechsci <37726532+amtechsci@users.noreply.github.com>
2023-10-28 14:25:43 +0530

    Update server.js
    m

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(6000, () => {
-  console.log('Server running on port 3000');
+  console.log('Server running on port 6000');
 });
